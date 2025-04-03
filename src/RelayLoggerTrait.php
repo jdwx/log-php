f97011ea0d28b4bee2ba0a029da10ff5b2da3d46
JDW <xs@jdw.sx>
2025-04-02 20:01:30 -0400

    Mark how long to retain.

--- src/RelayLoggerTrait.php
+++ src/RelayLoggerTrait.php
@@ -13,6 +13,8 @@ use Psr\Log\LoggerTrait;
 /**
  * @deprecated Use Psr\Log\LoggerTrait
  * @phpstan-ignore trait.unused
+ *
+ * Retain until the next minor version release after 2025-09-26.
  */
 trait RelayLoggerTrait {
 
