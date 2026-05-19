28dfe78506f8e56c3d3a7f91f9e07574842e07e2
JDW <xs@jdw.sx>
2026-05-19 14:02:38 -0400

    Add LoggerContainer class.

---
+++ src/LoggerContainer.php
@@ -0,0 +1,56 @@
+<?php
+
+
+declare( strict_types = 1 );
+
+
+namespace JDWX\Log;
+
+
+use Stringable;
+
+
+/**
+ * This is a direct proxy with no additional functionality.
+ *
+ * This class is intended to be used in situations where you need to initialize
+ * a LoggerInterface consumer before the mechanism you want it to use for logging
+ * is fully initialized.
+ *
+ * For example, this can help provide "last ditch" logging during program startup
+ * before the main logging mechanism is initialized without sticking early
+ * logging consumers with the last-ditch mechanism for the whole runtime.
+ */
+class LoggerContainer implements LoggerInterface {
+
+
+    use LoggerTrait;
+
+
+    private ?LoggerInterface $logger;
+
+
+    public function __construct( ?\Psr\Log\LoggerInterface $logger = null ) {
+        $this->setLogger( $logger );
+    }
+
+
+    public function getLogger() : ?LoggerInterface {
+        return $this->logger;
+    }
+
+
+    public function log( $level, Stringable|string $message, array $context = [] ) : void {
+        $this->logger?->log( $level, $message, $context );
+    }
+
+
+    public function setLogger( ?\Psr\Log\LoggerInterface $logger = null ) : void {
+        if ( ! is_null( $logger ) && ! $logger instanceof LoggerInterface ) {
+            $logger = new LoggerDecorator( $logger );
+        }
+        $this->logger = $logger;
+    }
+
+
+}
---
+++ tests/LoggerContainerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+
+declare( strict_types = 1 );
+
+
+namespace JDWX\Log\Tests;
+
+
+use JDWX\Log\BufferLogger;
+use JDWX\Log\LoggerContainer;
+use JDWX\Log\LoggerDecorator;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\AbstractLogger;
+use Psr\Log\LogLevel;
+use Stringable;
+
+
+class LoggerContainerTest extends TestCase {
+
+
+    public function testGetLogger() : void {
+        $log = new BufferLogger();
+        $container = new LoggerContainer();
+
+        self::assertSame( null, $container->getLogger() );
+        $container->setLogger( $log );
+        self::assertSame( $log, $container->getLogger() );
+    }
+
+
+    public function testLog() : void {
+        $buffer = new BufferLogger();
+        $container = new LoggerContainer();
+        $container->setLogger( $buffer );
+        $container->log( LogLevel::INFO, 'foo', [ 'baz' => 'qux' ] );
+        $entry = $buffer->shiftLogEx();
+        self::assertSame( LogLevel::INFO, $entry->level() );
+        self::assertSame( 'foo', $entry->message() );
+        self::assertSame( 'qux', $entry->context()[ 'baz' ] );
+    }
+
+
+    public function testSetLoggerWithPsrInterface() : void {
+        $log = new class extends AbstractLogger {
+
+
+            public function log( $level, string|Stringable $message, array $context = [] ) : void {}
+
+
+        };
+
+        $container = new LoggerContainer();
+        $container->setLogger( $log );
+        $decorator = $container->getLogger();
+        self::assertNotSame( $log, $decorator );
+        assert( $decorator instanceof LoggerDecorator );
+        self::assertSame( $log, $decorator->getLogger() );
+    }
+
+
+}
