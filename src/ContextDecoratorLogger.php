f659dd1383f26e9ab5567710b37949244fd2c28f
JDW <xs@jdw.sx>
2026-05-19 20:37:04 -0400

    Add method to ContextDecoratorLogger.
    - getContext()
    - hasContext()
    - mergeContext()

--- src/ContextDecoratorLogger.php
+++ src/ContextDecoratorLogger.php
@@ -16,6 +16,40 @@ class ContextDecoratorLogger extends AbstractContextDecoratorLogger {
     }
 
 
+    public function getContext( int|string $i_key, int|string|null $i_subKey = null ) : mixed {
+        if ( ! is_null( $i_subKey ) ) {
+            return $this->rExtraContext[ $i_key ][ $i_subKey ] ?? null;
+        }
+        return $this->rExtraContext[ $i_key ] ?? null;
+    }
+
+
+    public function hasContext( int|string $i_key, int|string|null $i_subKey = null ) : bool {
+        if ( ! array_key_exists( $i_key, $this->rExtraContext ) ) {
+            return false;
+        }
+        if ( is_null( $i_subKey ) ) {
+            return true;
+        }
+        if ( ! is_array( $this->rExtraContext[ $i_key ] ) ) {
+            return false;
+        }
+        return array_key_exists( $i_subKey, $this->rExtraContext[ $i_key ] );
+    }
+
+
+    public function mergeContext( int|string $i_key, string $i_subKey, mixed $i_context ) : void {
+        if ( ! array_key_exists( $i_key, $this->rExtraContext ) ) {
+            $this->setContext( $i_key, [ $i_subKey => $i_context ] );
+            return;
+        }
+        if ( ! is_array( $this->rExtraContext[ $i_key ] ) ) {
+            throw new \InvalidArgumentException( "Merging key {$i_key}:{$i_subKey} but {$i_key} is not an array." );
+        }
+        $this->rExtraContext[ $i_key ][ $i_subKey ] = $i_context;
+    }
+
+
     public function setContext( int|string $i_key, mixed $i_value ) : void {
         $this->rExtraContext[ $i_key ] = $i_value;
     }
--- tests/ContextDecoratorTest.php
+++ tests/ContextDecoratorTest.php
@@ -20,6 +20,43 @@ use Psr\Log\LogLevel;
 final class ContextDecoratorTest extends TestCase {
 
 
+    public function testGetContext() : void {
+        $buffer = new BufferLogger();
+        $decorator = new ContextDecoratorLogger( $buffer, [
+            'foo' => 'bar',
+            'baz' => [
+                'qux' => 'quux',
+            ],
+        ] );
+
+        self::assertSame( 'bar', $decorator->getContext( 'foo' ) );
+        self::assertSame( 'quux', $decorator->getContext( 'baz' )[ 'qux' ] );
+        self::assertSame( 'quux', $decorator->getContext( 'baz', 'qux' ) );
+        self::assertNull( $decorator->getContext( 'nonexistent' ) );
+        self::assertNull( $decorator->getContext( 'foo', 'nonexistent' ) );
+        self::assertNull( $decorator->getContext( 'baz', 'nonexistent' ) );
+    }
+
+
+    public function testHasContext() : void {
+        $buffer = new BufferLogger();
+        $decorator = new ContextDecoratorLogger( $buffer, [
+            'foo' => 'bar',
+            'baz' => [
+                'qux' => 'quux',
+            ],
+        ] );
+
+        self::assertTrue( $decorator->hasContext( 'foo' ) );
+        self::assertTrue( $decorator->hasContext( 'baz', 'qux' ) );
+        self::assertFalse( $decorator->hasContext( 'baz', 'quux' ) );
+        self::assertFalse( $decorator->hasContext( 'qux' ) );
+        self::assertFalse( $decorator->hasContext( 'nonexistent' ) );
+        self::assertFalse( $decorator->hasContext( 'foo', 'bar' ) );
+
+    }
+
+
     public function testLog() : void {
         $buffer = new BufferLogger();
         $decorator = new ContextDecoratorLogger( $buffer );
@@ -41,6 +78,29 @@ final class ContextDecoratorTest extends TestCase {
         self::assertArrayNotHasKey( 'bar', $log->context );
         self::assertSame( 'quux', $log->context[ 'qux' ] );
 
+    }
+
+
+    public function testMergeContext() : void {
+        $buffer = new BufferLogger();
+        $decorator = new ContextDecoratorLogger( $buffer );
+        $decorator->mergeContext( 'foo', 'bar', 'baz' );
+        $decorator->mergeContext( 'foo', 'qux', 'quux' );
+        $decorator->log( LogLevel::DEBUG, 'corge' );
+        $log = $buffer->shiftLogEx();
+
+        self::assertSame( LogLevel::DEBUG, $log->level );
+        self::assertSame( 'corge', $log->message );
+        self::assertSame( 'baz', $log->context[ 'foo' ][ 'bar' ] );
+        self::assertSame( 'quux', $log->context[ 'foo' ][ 'qux' ] );
+
+    }
+
+
+    public function testMergeContextForNotArray() : void {
+        $decorator = new ContextDecoratorLogger( new BufferLogger(), [ 'foo' => 'bar' ] );
+        $this->expectException( \InvalidArgumentException::class );
+        $decorator->mergeContext( 'foo', 'baz', 123 );
     }
 
 
